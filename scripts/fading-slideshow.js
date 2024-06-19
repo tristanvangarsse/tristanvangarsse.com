b97c616e654932abbeea8db0e60fb13064709a6c
TR1510 <tristanvangarsse@proton.me>
2024-06-19 21:49:30 +0200

    -

---
+++ scripts/fading-slideshow.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let slides = document.querySelectorAll('.slide');
+  let currentIndex = 0;
+  let slideInterval;
+  let hasScrolled = false;
+
+  function showNextSlide() {
+      slides[currentIndex].classList.remove('show');
+      currentIndex = (currentIndex + 1) % slides.length;
+      slides[currentIndex].classList.add('show');
+  }
+
+  function startSlideshow() {
+      if (!hasScrolled) {
+          slideInterval = setInterval(showNextSlide, 7000); // Change slide every 3 seconds
+          hasScrolled = true;
+      }
+  }
+
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 0 && !hasScrolled) {
+          startSlideshow();
+      }
+  });
+
+  // Initial display
+  slides[currentIndex].classList.add('show');
+});
