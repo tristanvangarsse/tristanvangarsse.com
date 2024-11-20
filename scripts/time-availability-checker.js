2e521badcf47d2121408b963e706664f0e5e01f4
TR1510 <tristanvangarsse@proton.me>
2024-11-20 17:44:04 +0100

    -

--- scripts/time-availability-checker.js
+++ scripts/time-availability-checker.js
@@ -1,7 +1,7 @@
 // Define availability times (in 24-hour format) for Brussels
 const availability = {
-  start: 16, // Available from 9:00
-  end: 20,  // Available until 18:00
+  start: 8, // Available from 9:00
+  end: 19,  // Available until 18:00
 };
 
 function updateStatus() {
