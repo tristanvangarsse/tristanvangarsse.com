2a28e75a844b46cc53c3bfdf10e51ebcc84a096b
TR1510 <tristanvangarsse@proton.me>
2025-04-07 21:12:02 +0200

    -

--- scripts/scroll-resize.js
+++ scripts/scroll-resize.js
@@ -1,9 +1,9 @@
 window.addEventListener("scroll", function () {
   if (window.innerWidth > 1500) { // Only run if screen width is greater than 1500px
     let scrollY = window.scrollY;
-    let minWidth = 50;
+    let minWidth = 46;
     let maxWidth = 65;
-    let growthRate = 0.03;
+    let growthRate = 0.04;
 
     let newWidth = Math.min(minWidth + scrollY * growthRate, maxWidth);
     
